edb872bfc9fde698ff03867b15eedea9756abcc8
amr11 <mneimneh.amer@gmail.com>
2025-07-25 10:10:18 -0500

    simplify build.ps1

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 meson setup --wrap-mode=forcefallback --buildtype=release build
 meson compile -C build
-meson install -C build --skip-subprojects="freetype2,pcre2,sdl2" --destdir ../pragtical/
+meson install -C build --skip-subprojects --destdir ../pragtical/
